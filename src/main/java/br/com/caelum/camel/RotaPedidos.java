5d104d90e3f335f28b2f9042bba02c22e97b0a74
Sérgio Santos <sergiorobsantos@gmail.com>
2021-04-11 00:18:38 -0300

    add initial files

---
+++ src/main/java/br/com/caelum/camel/RotaPedidos.java
@@ -0,0 +1,67 @@
+package br.com.caelum.camel;
+
+import org.apache.camel.CamelContext;
+import org.apache.camel.Exchange;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.component.http4.HttpMethods;
+import org.apache.camel.impl.DefaultCamelContext;
+
+public class RotaPedidos {
+
+	public static void main(String[] args) throws Exception {
+
+		CamelContext context = new DefaultCamelContext();
+//		context.addRoutes(new RouteBuilder() {
+//			@Override
+//			public void configure() throws Exception {
+//			from("file:pedidos?delay=5s&noop=true").
+//				split().xpath("/pedido/itens/item").
+//					log("${id} - ${body}").
+//				filter().xpath("/item/formato[text()='EBOOK']").
+//					log("${id} - ${body}").
+//				marshal().xmljson().
+//				log("${body}").
+//				setHeader(Exchange.FILE_NAME, simple("${file:name.noext}-${header.CamelSplitIndex}.json")).
+//			to("file:saida");
+//			}
+//		});
+
+//		context.addRoutes(new RouteBuilder() {
+//			@Override
+//			public void configure() throws Exception {
+//			from("file:pedidos?delay=5s&noop=true").
+//				split().xpath("/pedido/itens/item").
+//				log("${id} - ${body}").
+//				filter().xpath("/item/formato[text()='EBOOK']").
+//				log("${id} - ${body}").
+//				marshal().xmljson().
+//				log("${body}").
+//				setHeader(Exchange.HTTP_METHOD, HttpMethods.POST).
+//			to("http4://localhost:8080/webservices/ebook/item");
+//			}
+//		});
+
+		context.addRoutes(new RouteBuilder() {
+			@Override
+			public void configure() throws Exception {
+				from("file:pedidos?delay=5s&noop=true").
+						setProperty("pedidoId", xpath("/pedido/id/text()")).
+						setProperty("clienteId", xpath("/pedido/pagamento/email-titular/text()")).
+						split().xpath("/pedido/itens/item").
+						log("${id} - ${body}").
+						filter().xpath("/item/formato[text()='EBOOK']").
+						setProperty("ebookId", xpath("/item/livro/codigo/text()")).
+						log("${id} - ${body}").
+						marshal().xmljson().
+						log("${body}").
+						setHeader(Exchange.HTTP_METHOD, HttpMethods.GET).
+						setHeader(Exchange.HTTP_QUERY, simple("ebookId=${property.ebookId}&pedidoId=${property.pedidoId}&clienteId=${property.clienteId}")).
+						to("http4://localhost:8080/webservices/ebook/item");
+			}
+		});
+
+		context.start();
+		Thread.sleep(20000);
+		context.stop();
+	}	
+}
